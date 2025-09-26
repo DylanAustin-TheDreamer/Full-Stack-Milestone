e10d64d2920d26ae9c182d7b700e14ee40a0599e
DylanAustin-TheDreamer <austindylan0@gmail.com>
2025-09-26 10:03:54 +0100

    After a few error messages after trying to make migratiosn - copilot helped set up the settings for allauth and compatability for postgres

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    """Homepage view"""
+    return HttpResponse("<h1>Welcome to Your Python App Download Site!</h1><p>Your Django project is working!</p>")
